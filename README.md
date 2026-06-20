ddef1f7b9d61094f724badd3e802a388a85d85fa
Justin Herr <herrjustin0@gmail.com>
2026-06-19 20:54:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# C-Sharp-Players-Guide-5e-Solutions
+My solutions to the challenges in the C# Player's Guide 5th Edition
---
+++ TheThingNamer3000/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+// string a stores the answer to the question above  
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// string b stores the answer from the question above  
+string b = Console.ReadLine();
+// string c stores "of Doom"  
+string c = " of Doom";
+// string d stores "3000"  
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
+Console.WriteLine("The " + b + " " + a + " of Doom 3000!");
